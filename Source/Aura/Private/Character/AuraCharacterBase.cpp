f9bffe5ec77d4c4e19a2c83c13db9a6a8c6af123
Igwe Ugwoke <78117224+dasky77@users.noreply.github.com>
2024-03-31 13:29:01 -0600

    Player and Enemy Characters

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,21 @@
+// Copyright Vecury Interactive
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = false;
+
+}
+
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+
---
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -0,0 +1,5 @@
+// Copyright Vecury Interactive
+
+
+#include "Character/AuraEnemy.h"
+
